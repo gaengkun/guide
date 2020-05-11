88a78d4562c47bdd6b3adfa510de0adf45e57562
gaengkun <gaengkun@gmail.com>
2020-05-12 02:15:11 +0900

    sassbem 진행중

--- src/pages/Sassbem.js
+++ src/pages/Sassbem.js
@@ -1,5 +1,10 @@
 import React from "react";
+// import styled from "styled-components";
 // import icon_change from "../images/icon_change.png";
+import "../css/Sassbem.css";
+import "../css/bem.css";
+import PrevIcon from "../images/PrevIcon.svg";
+
 const Sassbem = () => {
   return (
     <div>
@@ -7,12 +12,17 @@ const Sassbem = () => {
         <h3 className="bem_tit">
           Sass + BEM <i></i>
         </h3>
-        {/* <img src={list_icon} className="bem_img" /> */}
-
         <div className="bem_text">리스트 영역 예시 </div>
       </section>
+
+      <div className="chas">
+        <div className="mobile_padding title--text">
+          <img src={PrevIcon} className="title--prev " alt="logo" />달 충전하기
+        </div>
+      </div>
     </div>
   );
 };
+
 
 export default Sassbem;
